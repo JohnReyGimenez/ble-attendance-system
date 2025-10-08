8341c5c162186b5fd0620d9ef9d4c7b5ecfd5c18
JohnReyGimenez <gimenezjr5@gmail.com>
2025-10-08 22:49:37 +0800

    added students model with data using resource generate

---
+++ db/migrate/20251008144857_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration[8.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :student_id
+      t.string :block
+
+      t.timestamps
+    end
+  end
+end

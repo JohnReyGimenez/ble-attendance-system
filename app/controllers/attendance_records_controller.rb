c6f26b464b0a47eaafbb6b4b4e34b210b984f4b7
JohnReyGimenez <gimenezjr5@gmail.com>
2025-10-09 21:39:21 +0800

    added index, show, create, update, destroy actions to attendance records controller

--- app/controllers/attendance_records_controller.rb
+++ app/controllers/attendance_records_controller.rb
@@ -1,2 +1,37 @@
 class AttendanceRecordsController < ApplicationController
+  def index
+    @attendance_records = AttendanceRecord.all
+    render json: @attendance_records
+  end
+
+  def show
+    @attendance_records = AttendanceRecord.find(params[:id])
+    render json: @attendance_records
+  end
+
+  def create
+    @attendance_records = AttendanceRecord.create(
+        timestamp: params[:timestamp],
+        status: params[:status],
+        student: params[:student]
+    )
+    render json: @attendance_records
+  end
+
+  def update
+    @attendance_records = AttendanceRecord.find(params[:id])
+    @secretMenuItem.update(
+      timestamp: params[:timestamp],
+      status: params[:status],
+      student: params[:student]
+    )
+    render json: @attendance_records
+  end
+
+  def destroy
+    @attendance_records = AttendanceRecord.all
+    @attendance_records = AttendanceRecord.find(params[:id])
+    @attendance_records.destroy
+     render json: @attendance_records
+  end
 end

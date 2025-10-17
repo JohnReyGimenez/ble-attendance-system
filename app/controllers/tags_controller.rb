8badd8cd5ddaffb9dc7ccc5af1c0a32b353163fa
JohnReyGimenez <gimenezjr5@gmail.com>
2025-10-17 22:30:03 +0800

    defined tag controller to create and update tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,30 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+    render json: @tags
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      render json: @tag, status: :created
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      render json: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:mac_address, :student_id)
+  end
 end

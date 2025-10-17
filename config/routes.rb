ce9f3fedc718810b0d4cb767a96e93b26bdeaed6
JohnReyGimenez <gimenezjr5@gmail.com>
2025-10-18 00:01:02 +0800

    fix: routes misplaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
-  resources :tags
-  resources :attendance_records
-  resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   namespace :api do
     namespace :v1 do
+      resources :students do
+        member do
+          put :assign_tag
+          put :unassign_tag
+        end
+      end
+
+      resources :tags
+      resources :attendance_records
       resources :attendances, only: [ :index, :create, :update ]
     end
   end
+
+  # Health check route (optional)
+  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
